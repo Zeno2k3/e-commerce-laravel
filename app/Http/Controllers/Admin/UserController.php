eff294e702ec5864739cb1d69d14f6228ffb625a
MinhQuan.dev <mquan592003@gmail.com>
2026-01-04 10:02:56 +0700

    fix controller User custommers

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,11 +3,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
+use App\Models\User;
 
 class UserController extends Controller {
     public function index() {
         // Lấy toàn bộ danh sách khách hàng từ bảng user
-        $users = DB::table('user')->get();
+        $users = User::where('role', 'user')->get();
+
         return view('admin.customers.index', compact('users'));
     }
 }
